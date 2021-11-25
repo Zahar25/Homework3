43311c4c19ec06e8cd25df07c628698ab4bb2735
Zahar25 <zpyshny@gmail.com>
2021-11-25 16:14:59 +0200

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,11 +1,15 @@
 #4. Створiть 3 рiзних функцiї (на ваш вибiр). Кожна з цих функцiй повинна повертати якийсь результат. Також створiть четверу ф-цiю, яка в тiлi викликає 3 попереднi, обробляє повернутий ними результат та також повертає результат. Таким чином ми будемо викликати 1 функцiю, а вона в своєму тiлi ще 3
 def func():
-    def info(name, age, city):
+    def info(name):
         print ("Your name is: ", name)
-        print("Your age is: ", age)
-        print("You live in: ", city)
         return name
+    info (name = input("Enter your name: "))
+    def info_2 (age):
+        print("Your age is: ", age)
         return age
-        return city
-    info(name = input(), age=input(), city = input())
+    info_2(age=input("How old are you?: "))
+    def info_3 (city):
+        print("You live in: ", city)
+        return (city)
+    info_3(city = input("Where do you live?: "))
 func()
