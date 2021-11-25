f966be6e457343cfe51be88ffa24972860608685
Zahar25 <zpyshny@gmail.com>
2021-11-25 18:08:37 +0200

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,12 +1,9 @@
 #2.Користувачем вводиться початковий і кінцевий рік. Створити цикл, який виведе всі високосні роки в цьому проміжку (границі включно)
-first_year = int(input("Year from: "))
-second_year = int(input("To: "))
-for year in range(first_year, second_year + 1):
-    if year % 400 == 0:
-        print("%d leap year" %year)
-    elif year % 100 == 0:
-            print("%d not leap" %year)
-    elif year % 4 == 0:
-            print("%d leap year" %year)
-    else:
-        print("%d not leap" %year)
+start = int(input("Enter start year: "))
+end = int(input("Enter end year: "))
+if start <= end:
+    leap_years = [str(x + start) for x in range(end-start) if x % 4 == 0 and x % 100 != 0]
+    leap_years[-1] += "."
+    print(f"Here is a list of leap years between {start} and {end}:\n{(','.join(leap_years))}")
+else:
+    print("Check your input years again!")
